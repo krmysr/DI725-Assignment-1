41525ead8729147d04d0acde2263a5c010b5d998
krmysr <krmysr@gmail.com>
2025-04-04 15:49:58 +0300

    filtered data based on negative, positive customer sentiments to better analyze it.

--- config/explore_data.py
+++ config/explore_data.py
@@ -18,25 +18,46 @@ print(df_test.columns)
 value_counts_test = df_test['customer_sentiment'].value_counts()
 value_counts_train = df_train['customer_sentiment'].value_counts()
 
+filtered_positive = df_train[df_train['customer_sentiment'] == 'positive']
+filtered_negative = df_train[df_train['customer_sentiment'] == 'negative']
+filtered_neutral = df_train[df_train['customer_sentiment'] == 'neutral']
 
-value_counts_test.plot(kind='bar')
+value_counts_positive = filtered_positive['issue_sub_category'].value_counts()
+value_counts_negative = filtered_negative['issue_sub_category'].value_counts()
 
-# Add labels and title
+
+# value_counts_test.plot(kind='bar')
+
+# # Add labels and title
+# plt.xlabel('Values')
+# plt.ylabel('Frequency')
+# plt.title('Value counts of customer sentiment')
+
+# # Display the plot
+# plt.tight_layout()
+# plt.show()
+
+# value_counts_train.plot(kind='bar')
+
+# # Add labels and title
+# plt.xlabel('Values')
+# plt.ylabel('Frequency')
+# plt.title('Value counts of customer sentiment')
+
+# # Display the plot
+# plt.tight_layout()
+# plt.show()
+
+value_counts_negative.plot()
 plt.xlabel('Values')
 plt.ylabel('Frequency')
-plt.title('Value counts of customer sentiment')
+plt.title('Value counts')
 
 # Display the plot
 plt.tight_layout()
 plt.show()
 
-value_counts_train.plot(kind='bar')
 
-# Add labels and title
-plt.xlabel('Values')
-plt.ylabel('Frequency')
-plt.title('Value counts of customer sentiment')
 
-# Display the plot
-plt.tight_layout()
-plt.show()
+
+
